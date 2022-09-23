e799e446c617c254ba0631a87fdf409f4785399f
Michael McMillan <mikemc26m@comcast.net>
2022-09-23 01:24:32 +0000

    added some cool javascript

--- code.js
+++ code.js
@@ -1,4 +1,18 @@
-let usersName = prompt("What is your name?");
-console.log(usersName);
-document.write("Well look at that, " + usersName + " loves crab too!");
+// let usersName = prompt("What is your name?");
+// console.log(usersName);
+// document.write("Well look at that, " + usersName + " loves crab too!");
+
+// declaring the function
+function greetUser(){
+    let usersName = prompt("What is your name?");
+    console.log(usersName);
+            return usersName;
+}
+const myName = greetUser();
+document.write("Hi " + myName + " ,you must love crab too!!");
+// invoking/calling the function
+// greetUser();
+
+
+
 
---
+++ new.js
@@ -0,0 +1,32 @@
+// function functionName(parameters){code to be executed - steps}
+// function functionName(paremeters){
+//     code to be executed - steps
+// }
+
+// declaring the function
+function greetUser(){
+    let usersName = prompt("What is your name?");
+    console.log(usersName);
+    document.wright("Hi = usersName + welcome");
+    return usersName;
+}
+
+// invoking/calling the function
+greetUser();
+
+// // function expression - not doing it this way right now
+// let newFunction = function(){
+//     console.log("Hello from inside the function expression");
+// }
+
+
+function customlert() {
+    if (userName == "Mike") {
+        alert("Hi Mike");
+    }   else if  
+}
+
+function addTwoNumbers(num1, num2){
+    return num1 + num2;
+}
+addTwoNumbers()
